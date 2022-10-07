d4e0a06d11b61f887e763e747ab86c41f16db4bf
Shannonmarie <69419374+Shanonbaker@users.noreply.github.com>
2022-10-07 21:34:52 +0530

    Create linearSearch.php

---
+++ linearSearch.php
@@ -0,0 +1,31 @@
+<?php
+// PHP code for linearly search x in arr[].
+// If x is present then return its location, Added with extra mines, thus making it shorter
+// otherwise return -1
+ 
+function search($arr, $x)
+{
+    $n = sizeof($arr);
+    for($i = 0; $i < $n; $i++)
+    {
+        if($arr[$i] == $x)
+            return $i;
+    }
+    return -1;
+}
+ 
+// Driver Code
+$arr = array(2, 3, 4, 10, 40);
+$x = 10;
+ 
+// Function call
+$result = search($arr, $x);
+if($result == -1)
+    echo "Element is not present in array";
+else
+    echo "Element is present at index " ,
+                                 $result;
+ 
+// This code is contributed
+// by jit_t
+?>
